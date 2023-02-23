ed64673a0f828f6d5e48bebe9a138fa911dccf40
tcmak217 <tcmak217@outlook.com>
2023-02-24 00:08:21 +0800

    feat: add font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontSize: "15px",
       fontFamily: {
         sans: ["Overpass", "sans-serif"],
       },
