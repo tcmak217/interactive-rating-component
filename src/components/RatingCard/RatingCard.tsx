9e4ab2116715e1c795637e3793adc52659835a82
tcmak217 <tcmak217@outlook.com>
2023-02-24 23:55:40 +0800

    fix: change to use gap instead of margin to add space between elements

--- src/components/RatingCard/RatingCard.tsx
+++ src/components/RatingCard/RatingCard.tsx
@@ -10,22 +10,22 @@ function RatingCard({
 }) {
   return (
     <div className="bg-[hsl(215,20%,15%)] w-[320px] rounded-3xl">
-      <div className="w-full h-full flex flex-col p-8">
-        <div className="flex items-center mb-2">
+      <div className="w-full h-full flex flex-col p-8 gap-4">
+        <div className="flex items-center">
           <div className="w-9 h-9 rounded-full bg-dark-blue flex justify-center items-center">
             <img className="w-3 h-3" src={startIcon} alt="star icon" />
           </div>
         </div>
-        <div className="flex items-center my-2">
+        <div className="flex items-center">
           <div className="text-white text-2xl font-bold">How did we do?</div>
         </div>
-        <div className="flex items-center my-2">
+        <div className="flex items-center">
           <div className="text-light-grey text-sm">
             Please let us know how we did with your support request. All feedback is appreciated to
             help us improve our offering!
           </div>
         </div>
-        <div className="flex flex-row justify-between items-center my-4">
+        <div className="flex flex-row justify-between items-center">
           <button
             className={`w-10 h-10 text-light-grey bg-dark-blue active:bg-orange active:text-white rounded-full ${
               rating === 51 ? "bg-light-grey text-white" : ""
