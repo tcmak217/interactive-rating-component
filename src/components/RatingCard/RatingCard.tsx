6ae2a129bb37b6ae077f5cdc990aff1615a8977e
tcmak217 <tcmak217@outlook.com>
2023-02-24 12:24:27 +0800

    feat: add layout to the rating card

--- src/components/RatingCard/RatingCard.tsx
+++ src/components/RatingCard/RatingCard.tsx
@@ -4,16 +4,18 @@ import startIcon from "../../images/icon-star.svg"
 function RatingCard() {
   return (
     <div className="bg-dark-blue max-w-[320px] w-[90%] h-[400px] rounded-3xl">
-      <div>
-        <img src={startIcon} alt="" />
+      <div className="w-full h-full flex flex-col p-8">
+        <div className="grow">
+          <img src={startIcon} alt="star icon" />
+        </div>
+        <div className="text-white grow">How did we do?</div>
+        <div className="text-light-grey grow">
+          Please let us know how we did with your support request.All feedback is appreciated to
+          help us improve our offering!
+        </div>
+        <div className="text-light-grey grow">1 2 3 4 5</div>
+        <div className="text-white grow">Submit</div>
       </div>
-      <div className="text-white">How did we do?</div>
-      <div className="text-light-grey">
-        Please let us know how we did with your support request.All feedback is appreciated to help
-        us improve our offering!
-      </div>
-      <div className="text-light-grey">1 2 3 4 5</div>
-      <div className="text-white">Submit</div>
     </div>
   )
 }
