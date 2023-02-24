ef16e456343e85bc27eb0e245511c5ad8d1af08c
tcmak217 <tcmak217@outlook.com>
2023-02-24 11:00:28 +0800

    feat: add color to text

--- src/components/RatingCard/RatingCard.tsx
+++ src/components/RatingCard/RatingCard.tsx
@@ -3,13 +3,13 @@ import React from "react"
 function RatingCard() {
   return (
     <div className="">
-      <div className="">How did we do?</div>
-      <div>
+      <div className="text-white">How did we do?</div>
+      <div className="text-light-grey">
         Please let us know how we did with your support request.All feedback is appreciated to help
         us improve our offering!
       </div>
-      <div>1 2 3 4 5</div>
-      <div>Submit</div>
+      <div className="text-light-grey">1 2 3 4 5</div>
+      <div className="text-white">Submit</div>
     </div>
   )
 }
