745b20c229c5dd8d37fea34ec2080744da0ae1ee
tcmak217 <tcmak217@outlook.com>
2023-02-25 15:28:35 +0800

    feat: add thank you card implementation

--- src/components/ThanksCard/ThanksCard.tsx
+++ src/components/ThanksCard/ThanksCard.tsx
@@ -1,10 +1,22 @@
 import React from "react"
+import thanksImage from "../../images/illustration-thank-you.svg"
 
 function ThanksCard({ rating }: { rating: number | null }) {
   return (
-    <div>
-      You selected out of 5 Thank you! We appreciate you taking the time to give a rating. If you
-      ever need more support, don't hesitate to get in touch!
+    <div className="bg-[hsl(215,20%,15%)] w-[320px] h-[360px] rounded-3xl">
+      <div className="w-full h-full flex flex-col p-8 gap-4 items-center justify-center">
+        <div className="w-[150px]">
+          <img src={thanksImage} alt="thank you image" />
+        </div>
+        <div>
+          <div className="text-orange text-sm">You selected {rating} out of 5</div>
+        </div>
+        <div className="text-white text-2xl font-bold">Thank you!</div>
+        <div className="text-light-grey text-sm text-center">
+          We appreciate you taking the time to give a rating. If you ever need more support, don't
+          hesitate to get in touch!
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/PageRating.tsx
+++ src/pages/PageRating.tsx
@@ -17,17 +17,15 @@ function PageRating() {
   return (
     <>
       <div className="w-full h-screen bg-very-dark-blue flex justify-center items-center">
-        <div className="bg-[hsl(215,20%,15%)] w-[320px] min-h-[356px] rounded-3xl">
-          {!isSubmitted ? (
-            <Rating
-              rating={rating}
-              handleSetRating={handleSetRating}
-              handleSetIsSubmitted={handleSetIsSubmitted}
-            ></Rating>
-          ) : (
-            <ThanksCard rating={rating}></ThanksCard>
-          )}
-        </div>
+        {!isSubmitted ? (
+          <Rating
+            rating={rating}
+            handleSetRating={handleSetRating}
+            handleSetIsSubmitted={handleSetIsSubmitted}
+          ></Rating>
+        ) : (
+          <ThanksCard rating={rating}></ThanksCard>
+        )}
       </div>
     </>
   )
